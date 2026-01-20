430fe423fdc2c0011331c46c0cb00d179f0eaf75
sujithgoud2210 <sujith200322@gmail.com>
2026-01-20 12:36:19 +0530

    feat:second commit

---
+++ src/com/stschool/java/fundamentals/CountDigits.java
@@ -0,0 +1,18 @@
+package com.stschool.java.fundamentals;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int number, count = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        number = sc.nextInt();
+        while(number>0)
+        {
+            number=number/10;
+            count=count+1;
+        }
+        System.out.println("count in the given digit is : "+count);
+    }
+}
---
+++ src/com/stschool/java/fundamentals/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package com.stschool.java.fundamentals;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number, result = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        number = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            result = number * i;
+            System.out.println(number + "*" + i + "=" + result);
+        }
+    }
+}
