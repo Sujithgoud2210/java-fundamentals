20c18c8014c229b0f54690c72ad4b3cff485598c
sujithgoud2210 <sujith200322@gmail.com>
2026-01-20 14:24:56 +0530

    feat: Third commit

---
+++ src/com/stschool/java/fundamentals/level2/PalindromeNUmber.java
@@ -0,0 +1,27 @@
+package com.stschool.java.fundamentals.level2;
+
+import java.util.Scanner;
+
+public class PalindromeNUmber {
+    public static void main(String[] args) {
+        int number, digit,reverse=0,original;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        number = sc.nextInt();
+        original=number;
+        while(number>0)
+        {
+            digit=number%10;
+            reverse=reverse*10+digit;
+            number=number/10;
+        }
+        if(original==reverse)
+        {
+            System.out.println("palindrome");
+        }
+        else
+        {
+            System.out.println("not a palindrome");
+        }
+    }
+}
