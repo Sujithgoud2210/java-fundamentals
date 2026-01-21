6eeb510783dc222dff81296655388d1fcb50a4e9
sujithgoud2210 <sujith200322@gmail.com>
2026-01-21 15:55:13 +0530

    feat:Third commit

--- src/com/java/fundamentals/HelloWorld.java
+++ src/com/java/fundamentals/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.java.fundamentals.level1;
+package com.java.fundamentals;
 
 public class HelloWorld {
     public static void main(String[] args) {
---
+++ src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,14 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    public static void main(String[] args) {
+        byte age = 22;
+        System.out.println("Age : " + age);
+        short price = 1034;
+        System.out.println("price : " + price);
+        int salary= 45000;
+        System.out.println("salary = " + salary);
+        long number = 799704717012L;
+        System.out.println("number : " + number);
+    }
+}
