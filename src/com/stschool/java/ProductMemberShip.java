b81c221ebaecaaae3ba94efd171b5c4181bdf3be
sujithgoud2210 <sujith200322@gmail.com>
2026-01-23 15:59:53 +0530

    fifth commit

---
+++ src/com/stschool/java/ProductMemberShip.java
@@ -0,0 +1,50 @@
+package com.stschool.java;
+
+import java.util.Scanner;
+
+public class ProductMemberShip {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int productPrice, quantity;
+        float discountPercentage, shippingCharges, centralTax, stateTax, finalPrice, DiscountedPrice, discountAmount, stateTaxAmount, centralTaxAmount, finalBill, totalSaved, savedAmount;
+        centralTax = 2.5f;
+        stateTax = 2.5f;
+        System.out.print("enter product price = ");
+        productPrice = sc.nextInt();
+        System.out.print("enter membership details");
+        System.out.println(" S for silver ");
+        System.out.println("G for gold");
+        System.out.println("D for diamond");
+        System.out.println("else you dont have membership");
+        char membership = sc.next().charAt(0);
+        if (membership == 'S') {
+            discountPercentage = 10;
+            discountAmount = (productPrice * discountPercentage) / 100;
+            shippingCharges = 50;
+        } else if (membership == 'G') {
+            discountPercentage = 15;
+            discountAmount = (productPrice * discountPercentage) / 100;
+            shippingCharges = 20;
+        } else if (membership == 'D') {
+            discountPercentage = 20;
+            discountAmount = (productPrice * discountPercentage) / 100;
+            shippingCharges = 0;
+        }
+        System.out.println("product price = " + productPrice);
+        DiscountedPrice = productPrice - discountAmount;
+        System.out.println("Discount Amount = " + discountAmount);
+        stateTaxAmount = (DiscountedPrice * stateTax) / 100;
+        centralTaxAmount = (DiscountedPrice * centralTax) / 100;
+        finalPrice = DiscountedPrice + centralTaxAmount + stateTaxAmount;
+        System.out.println("State Tax = " + stateTax);
+        System.out.println("Central Tax = " + centralTax);
+        System.out.println("Final Product Price = " + finalPrice);
+        savedAmount = productPrice - finalPrice;
+        System.out.print("enter quantity = ");
+        quantity = sc.nextInt();
+        finalBill = finalPrice * quantity + shippingCharges;
+        totalSaved = quantity * savedAmount;
+        System.out.println("Final  Bill = " + finalBill);
+        System.out.println("Total Saved = " + totalSaved);
+    }
+}
