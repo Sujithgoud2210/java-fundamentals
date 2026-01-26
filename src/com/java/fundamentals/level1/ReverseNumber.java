512425e8bbb63efbc980778751f594bc855ce29c
sujithgoud2210 <sujith200322@gmail.com>
2026-01-26 21:36:00 +0530

    sixth commit

--- src/com/java/fundamentals/level1/ReverseNumber.java
+++ src/com/java/fundamentals/level1/ReverseNumber.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class ReverseNumber {
     public static void main(String[] args) {
         int number;
+        int remainder, reverse=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number");
         number = sc.nextInt();
-        for (int i = number; i >= 1; i--)
-            System.out.println(i);
+        while (number > 0) {
+            remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number=number/10;
+        }
+        System.out.println(reverse);
     }
 }
