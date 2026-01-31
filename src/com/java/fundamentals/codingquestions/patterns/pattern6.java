4364ab6fe5e83917d2c60ce36f69684f352c6a01
sujithgoud2210 <sujith200322@gmail.com>
2026-01-31 23:34:11 +0530

    eleventh commit

--- src/com/java/fundamentals/codingquestions/patterns/pattern6.java
+++ src/com/java/fundamentals/codingquestions/patterns/pattern6.java
@@ -2,7 +2,7 @@ package com.java.fundamentals.codingquestions.patterns;
 
 import java.util.Scanner;
 
-public class pattern5 {
+public class pattern6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number;
@@ -10,16 +10,9 @@ public class pattern5 {
         number = sc.nextInt();
         for (int row = 1; row <= number; row++) {
             for (int column = 1; column <= number-row+1; column++) {
-                System.out.print("*"+" ");
+                System.out.print(column+" ");
             }
             System.out.println();
         }
     }
 }
-//enter a number
-//5
-//        * * * * *
-//        * * * *
-//        * * *
-//        * *
-//        *
